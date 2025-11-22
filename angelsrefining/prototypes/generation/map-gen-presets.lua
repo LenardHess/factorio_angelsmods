9e23f43cf2410b70adf144e70e5c765b18a00452
Lenard Hess <lenard@rrhess.de>
2025-11-22 14:40:25 +0100

    angelsrefining: Create a new default map generation setting
    This was intended to replace the "Angel's enemies slider", but currently
    this is not feasible.
    
    In space age, we actually can't do what the slider did and fixup the
    enemy spawn rate to 0, as you need spawners to progress (capture a
    spawner). The alternative would be peaceful mode or no enemies mode.
    However neither of these can be enabled by default, they can only be
    changed in (non-default) presets. That leaves this approach impossible.

--- angelsrefining/prototypes/generation/map-gen-presets.lua
+++ angelsrefining/prototypes/generation/map-gen-presets.lua
@@ -1,16 +1,38 @@
 if mods["angelsexploration"] then
   -- angels exploration takes care of this
 else
-  local map_settings = data.raw["map-settings"]["map-settings"]
-  map_settings.pollution.enabled = false
-  map_settings.enemy_evolution.enabled = false
-  map_settings.enemy_expansion.enabled = false
 
-  local probability = data.raw["noise-expression"]["enemy_base_probability"]
-  probability.local_expressions["old_probability"] = probability.expression
-  probability.local_expressions["angels_biter_slider"] = "if(var('control:angels-biter-slider:size') >= 6, 1, 0)"
-  probability.expression = "old_probability * angels_biter_slider"
-  data.raw.planet["nauvis"].map_gen_settings.autoplace_controls["angels-biter-slider"] = {}
+  -- log(serpent.block(data.raw["map-gen-presets"]))
+  -- data.raw["map-gen-presets"]["default"]["angels-default"] = {
+  --   order = "a", -- This moves us in front of the game's default
+  --   advanced_settings = {
+  --     pollution = {
+  --       enabled = false,
+  --     },
+  --     enemy_evolution = {
+  --       enabled = false,
+  --     },
+  --     enemy_expansion = {
+  --       enabled = false,
+  --     },
+  --   },
+  -- }
+  -- log(serpent.block(data.raw["map-gen-presets"]))
+
+  -- data.raw["map-gen-presets"]["default"]["angels-default"] = table.deepcopy(data.raw["map-gen-presets"]["default"]["default"])
+  -- data.raw["map-gen-presets"]["default"]["default"]["default"] = false
+  -- data.raw["map-gen-presets"]["default"].name = "angels-default"
+
+  -- local map_settings = data.raw["map-settings"]["map-settings"]
+  -- map_settings.pollution.enabled = false
+  -- map_settings.enemy_evolution.enabled = false
+  -- map_settings.enemy_expansion.enabled = false
+
+  -- local probability = data.raw["noise-expression"]["enemy_base_probability"]
+  -- probability.local_expressions["old_probability"] = probability.expression
+  -- probability.local_expressions["angels_biter_slider"] = "if(var('control:angels-biter-slider:size') >= 6, 1, 0)"
+  -- probability.expression = "old_probability * angels_biter_slider"
+  -- data.raw.planet["nauvis"].map_gen_settings.autoplace_controls["angels-biter-slider"] = {}
 
   data:extend({
     {
